c281a2761ed678610fbff7c02e8a733cc026e8c9
clarence <clarencewu2@gmail.com>
2019-10-16 00:09:44 +0800

    项目初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
+// 这个文件是 Angular 根模块，告诉 Angular 如何组装应用
+
+
+// BroserModule，浏览器解析的模块
 import { BrowserModule } from '@angular/platform-browser';
+// Angular 核心模块
 import { NgModule } from '@angular/core';
-
+// 根组件
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
+// @NgModule 装饰器，@NgModule 接受一个元数据对象，告诉 Angular 如何编译和启动应用
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [         /*配置当前项目运行的组件*/
+    AppComponent, NewsComponent, HomeComponent, HeaderComponent
   ],
-  imports: [
+  imports: [              /*配置当前模块运行依赖的其他模块*/
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [],          /*配置项目所需要的服务*/
+  bootstrap: [AppComponent]   /*指定应用的主视图（称为根组件）通过引导根 AppModule 来启动应用，这里一般写的是跟组件*/
 })
+
+// 根模块不需要导出任何东西，因为其他组件不需要导入根模块
 export class AppModule { }
