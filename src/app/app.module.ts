35c1250d0199897385f16093f6898c8e7604f471
clarence <clarencewu2@gmail.com>
2019-10-16 23:45:19 +0800

    搜索缓存数据

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 // Angular 核心模块
 import { NgModule } from '@angular/core';
+// 引入表单相关模块 才可以用双向数据绑定
+import {FormsModule} from '@angular/forms';
+
 // 根组件
 import { AppComponent } from './app.component';
-import { NewsComponent } from './components/news/news.component';
-import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
+import { SearchComponent } from './components/search/search.component';
+import { from } from 'rxjs';
 
 // @NgModule 装饰器，@NgModule 接受一个元数据对象，告诉 Angular 如何编译和启动应用
 @NgModule({
   declarations: [         /*配置当前项目运行的组件*/
-    AppComponent, NewsComponent, HomeComponent, HeaderComponent
+    AppComponent, SearchComponent
   ],
   imports: [              /*配置当前模块运行依赖的其他模块*/
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],          /*配置项目所需要的服务*/
   bootstrap: [AppComponent]   /*指定应用的主视图（称为根组件）通过引导根 AppModule 来启动应用，这里一般写的是跟组件*/
